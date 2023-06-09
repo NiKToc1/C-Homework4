14aa22b8ac6a8b79304ab94ae31738f4d2039290
NiKToc <niktoc20@gmail.com>
2023-06-09 15:21:15 +0500

    task 27

---
+++ task 27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+int result = 0;
+
+while (A > 0)
+{
+    int chislo = A % 10;
+    A = A / 10;
+    result = result + chislo;
+}
+
+Console.WriteLine($"Сумма цифр числа: {result}");
